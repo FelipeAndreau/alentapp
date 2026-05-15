e91ffdaa045d1a4fcb0c971f3fcc48fece6d7e91
MaxiCarpi <maximocarpignano@gmail.com>
2026-05-15 20:26:43 -0300

    feat(sports): agrega ruta /sports en el router del frontend

--- packages/web/src/routes.ts
+++ packages/web/src/routes.ts
@@ -1,26 +1,31 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from 'react-router';
 
-import { MembersView } from "./views/Members";
-import { HomeView } from "./views/Home";
-import { PaymentsView } from "./views/Payments";
-import Layout from "./Layout";
+import { MembersView } from './views/Members';
+import { HomeView } from './views/Home';
+import { PaymentsView } from './views/Payments';
+import { SportsView } from './views/Sports';
+import Layout from './Layout';
 
 export let router = createBrowserRouter([
-  {
-    Component: Layout,
-    children: [
-      {
-        path: "/",
-        Component: HomeView,
-      },
-      {
-        path: "/members",
-        Component: MembersView,
-      },
-      {
-        path: "/payments",
-        Component: PaymentsView,
-      },
-    ],
-  },
+    {
+        Component: Layout,
+        children: [
+            {
+                path: '/',
+                Component: HomeView,
+            },
+            {
+                path: '/members',
+                Component: MembersView,
+            },
+            {
+                path: '/payments',
+                Component: PaymentsView,
+            },
+            {
+                path: '/sports',
+                Component: SportsView,
+            },
+        ],
+    },
 ]);
