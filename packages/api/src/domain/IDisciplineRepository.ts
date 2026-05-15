c5e4d09d61244b727ea11fd6835d00898594efc4
fiuzapedropcs12-nitizen <fiuzapedropcs12@gmail.com>
2026-05-14 22:37:43 -0300

    feat(domain): agrega puerto IDisciplineRepository con métodos create y getAll

---
+++ packages/api/src/domain/IDisciplineRepository.ts
@@ -0,0 +1,6 @@
+import { DisciplineDTO, CreateDisciplineRequest } from '@alentapp/shared';
+
+export interface IDisciplineRepository {
+    create(data: CreateDisciplineRequest): Promise<DisciplineDTO>;
+    getAll(): Promise<DisciplineDTO[]>;
+}
