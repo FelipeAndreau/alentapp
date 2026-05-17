ed2dcb5d56cc778a6a441c6a409b1b1a0f5be800
FelipeAndreau <felipe.andreau@alu.frlp.utn.edu.ar>
2026-05-17 20:27:54 -0300

    fix: Marcas de conflicto

--- packages/api/src/domain/disciplines/DisciplineRepository.ts
+++ packages/api/src/domain/disciplines/DisciplineRepository.ts
@@ -2,12 +2,10 @@ import { DisciplineDTO, CreateDisciplineRequest } from '@alentapp/shared';
 
 export interface DisciplineRepository {
     create(data: CreateDisciplineRequest): Promise<DisciplineDTO>;
-<<<<<<< HEAD:packages/api/src/domain/disciplines/DisciplineRepository.ts
     findAll(): Promise<DisciplineDTO[]>;
-=======
     getAll(): Promise<DisciplineDTO[]>;
     findById(id: string): Promise<DisciplineDTO | null>;
     update(data: DisciplineDTO): Promise<DisciplineDTO>;
     delete(id: string): Promise<void>;
->>>>>>> origin/feature/discipline-delete:packages/api/src/domain/IDisciplineRepository.ts
+ origin/feature/discipline-delete:packages/api/src/domain/IDisciplineRepository.ts
 }
