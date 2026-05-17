49e694f21dcdaf3200ad2e289bbbd12c161b1e9f
FelipeAndreau <felipe.andreau@alu.frlp.utn.edu.ar>
2026-05-17 20:34:53 -0300

    fix: findall discipline

--- packages/api/src/domain/disciplines/DisciplineRepository.ts
+++ packages/api/src/domain/disciplines/DisciplineRepository.ts
@@ -2,7 +2,7 @@ import { DisciplineDTO, CreateDisciplineRequest } from '@alentapp/shared';
 
 export interface DisciplineRepository {
     create(data: CreateDisciplineRequest): Promise<DisciplineDTO>;
-    getAll(): Promise<DisciplineDTO[]>;
+    findAll(): Promise<DisciplineDTO[]>;
     findById(id: string): Promise<DisciplineDTO | null>;
     update(data: DisciplineDTO): Promise<DisciplineDTO>;
     delete(id: string): Promise<void>;
